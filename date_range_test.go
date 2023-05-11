cfc45c6dcb5ca7dd153689fb66df1c88f6388d82
Wei Chen <smile.v.chen@gmail.com>
2023-05-11 18:11:38 +0900

    Add support of week as cli argument (#3)
    * Add usage description for cli argument of week
    * Exchange order of command and flag in usage description of cli
    * Change and add tests for week argument in cli
    * Implement week argument in cli

--- date_range_test.go
+++ date_range_test.go
@@ -7,9 +7,9 @@ import (
 )
 
 func TestNewDateRange_StartDateAndEndDate(t *testing.T) {
-	r, err := NewDateRange("2023-05-01", "2023-05-07", 0)
+	r, err := NewDateRange("2023-05-01", "2023-05-07", 0, 0)
 	if err != nil {
-		t.Fatalf("NewDateRange(\"2023-05-01\", \"2023-05-07\", 0), expected none error, got %s", err)
+		t.Fatalf("NewDateRange(\"2023-05-01\", \"2023-05-07\", 0, 0), expected none error, got %s", err)
 	}
 	start, _ := time.Parse(time.DateOnly, "2023-05-01")
 	if !r.Start.Equal(start) {
@@ -22,9 +22,9 @@ func TestNewDateRange_StartDateAndEndDate(t *testing.T) {
 }
 
 func TestNewDateRange_StartDateAndDay(t *testing.T) {
-	r, err := NewDateRange("2023-05-01", "", 6)
+	r, err := NewDateRange("2023-05-01", "", 6, 0)
 	if err != nil {
-		t.Fatalf("NewDateRange(\"2023-05-01\", \"\", 6), expected none error, got %s", err)
+		t.Fatalf("NewDateRange(\"2023-05-01\", \"\", 6, 0), expected none error, got %s", err)
 	}
 	start, _ := time.Parse(time.DateOnly, "2023-05-01")
 	if !r.Start.Equal(start) {
@@ -36,10 +36,40 @@ func TestNewDateRange_StartDateAndDay(t *testing.T) {
 	}
 }
 
-func TestNewDateRange_StartDateAndEndDateAndDay(t *testing.T) {
-	r, err := NewDateRange("2023-05-01", "2023-05-07", 20)
+func TestNewDateRange_StartDateAndWeek(t *testing.T) {
+	r, err := NewDateRange("2023-05-01", "", 0, 2)
 	if err != nil {
-		t.Fatalf("NewDateRange(\"2023-05-01\", \"2023-05-07\", 20), expected none error, got %s", err)
+		t.Fatalf("NewDateRange(\"2023-05-01\", \"\", 0, 1), expected none error, got %s", err)
+	}
+	start, _ := time.Parse(time.DateOnly, "2023-05-01")
+	if !r.Start.Equal(start) {
+		t.Fatalf("Start date of Date Range should be %s but %s", start, r.Start)
+	}
+	end, _ := time.Parse(time.DateOnly, "2023-05-14")
+	if !r.End.Equal(end) {
+		t.Fatalf("End date of Date Range should be %s but %s", end, r.End)
+	}
+}
+
+func TestNewDateRange_StartDateAndEndDateAndDay(t *testing.T) {
+	r, err := NewDateRange("2023-05-01", "2023-05-07", 20, 0)
+	if err != nil {
+		t.Fatalf("NewDateRange(\"2023-05-01\", \"2023-05-07\", 20, 0), expected none error, got %s", err)
+	}
+	start, _ := time.Parse(time.DateOnly, "2023-05-01")
+	if !r.Start.Equal(start) {
+		t.Fatalf("Start date of Date Range should be %s but %s", start, r.Start)
+	}
+	end, _ := time.Parse(time.DateOnly, "2023-05-07")
+	if !r.End.Equal(end) {
+		t.Fatalf("End date of Date Range should be %s but %s", end, r.End)
+	}
+}
+
+func TestNewDateRange_StartDateAndEndDateAndWeek(t *testing.T) {
+	r, err := NewDateRange("2023-05-01", "2023-05-07", 0, 2)
+	if err != nil {
+		t.Fatalf("NewDateRange(\"2023-05-01\", \"2023-05-07\", 0, 2), expected none error, got %s", err)
 	}
 	start, _ := time.Parse(time.DateOnly, "2023-05-01")
 	if !r.Start.Equal(start) {
@@ -52,63 +82,70 @@ func TestNewDateRange_StartDateAndEndDateAndDay(t *testing.T) {
 }
 
 func TestNewDateRange_InvalidDateRange(t *testing.T) {
-	_, err := NewDateRange("2023-05-01", "2023-04-30", 0)
+	_, err := NewDateRange("2023-05-01", "2023-04-30", 0, 0)
 	if !errors.Is(err, ErrInvalidDateRange) {
-		t.Fatalf("NewDateRange(\"2023-05-01\", \"2023-04-30\", 0), expected %s, got %s", ErrInvalidDateRange, err)
+		t.Fatalf("NewDateRange(\"2023-05-01\", \"2023-04-30\", 0, 0), expected %s, got %s", ErrInvalidDateRange, err)
 	}
 }
 
 func TestNewDateRange_NoneOfArgument(t *testing.T) {
-	_, err := NewDateRange("", "", 0)
+	_, err := NewDateRange("", "", 0, 0)
 	if !errors.Is(err, ErrInvalidArgument) {
 		t.Fatalf("NewDateRange(\"\", \"\", 0), expected %s, got %s", ErrInvalidArgument, err)
 	}
 }
 
 func TestNewDateRange_OnlyArgumentOfStartDate(t *testing.T) {
-	_, err := NewDateRange("2023-05-01", "", 0)
+	_, err := NewDateRange("2023-05-01", "", 0, 0)
 	if !errors.Is(err, ErrInvalidArgument) {
-		t.Fatalf("NewDateRange(\"2023-05-01\", \"\", 0), expected %s, got %s", ErrInvalidArgument, err)
+		t.Fatalf("NewDateRange(\"2023-05-01\", \"\", 0, 0), expected %s, got %s", ErrInvalidArgument, err)
 	}
 }
 
 func TestNewDateRange_OnlyArgumentOfEndDate(t *testing.T) {
-	_, err := NewDateRange("", "2023-05-01", 0)
+	_, err := NewDateRange("", "2023-05-01", 0, 0)
 	if !errors.Is(err, ErrInvalidArgument) {
-		t.Fatalf("NewDateRange(\"\", \"2023-05-01\", 0), expected %s, got %s", ErrInvalidArgument, err)
+		t.Fatalf("NewDateRange(\"\", \"2023-05-01\", 0, 0), expected %s, got %s", ErrInvalidArgument, err)
 	}
 }
 
 func TestNewDateRange_OnlyArgumentOfDay(t *testing.T) {
-	_, err := NewDateRange("", "", 6)
+	_, err := NewDateRange("", "", 6, 0)
 	if !errors.Is(err, ErrInvalidArgument) {
-		t.Fatalf("NewDateRange(\"\", \"\", 6), expected %s, got %s", ErrInvalidArgument, err)
+		t.Fatalf("NewDateRange(\"\", \"\", 6, 0), expected %s, got %s", ErrInvalidArgument, err)
+	}
+}
+
+func TestNewDateRange_OnlyArgumentOfWeek(t *testing.T) {
+	_, err := NewDateRange("", "", 0, 2)
+	if !errors.Is(err, ErrInvalidArgument) {
+		t.Fatalf("NewDateRange(\"\", \"\", 0, 2), expected %s, got %s", ErrInvalidArgument, err)
 	}
 }
 
 func TestNewDateRange_WithoutArgumentOfStartDate(t *testing.T) {
-	_, err := NewDateRange("", "2023-05-01", 6)
+	_, err := NewDateRange("", "2023-05-01", 6, 2)
 	if !errors.Is(err, ErrInvalidArgument) {
-		t.Fatalf("NewDateRange(\"\", \"2023-05-01\", 6), expected %s, got %s", ErrInvalidArgument, err)
+		t.Fatalf("NewDateRange(\"\", \"2023-05-01\", 6, 2), expected %s, got %s", ErrInvalidArgument, err)
 	}
 }
 
 func TestNewDateRange_InvalidFormatOfStartDate(t *testing.T) {
-	_, err := NewDateRange("Invalid Date Format", "Invalid Date Format", 0)
+	_, err := NewDateRange("Invalid Date Format", "Invalid Date Format", 0, 0)
 	if !errors.Is(err, ErrInvalidDateFormat) {
-		t.Fatalf("NewDateRange(\"Invalid Date Format\", \"2023-05-01\", 0), expected %s, got %s", ErrInvalidDateFormat, err)
+		t.Fatalf("NewDateRange(\"Invalid Date Format\", \"2023-05-01\", 0, 0), expected %s, got %s", ErrInvalidDateFormat, err)
 	}
 }
 
 func TestNewDateRange_InvalidFormatOfEndDate(t *testing.T) {
-	_, err := NewDateRange("Invalid Date Format", "Invalid Date Format", 0)
+	_, err := NewDateRange("Invalid Date Format", "Invalid Date Format", 0, 0)
 	if !errors.Is(err, ErrInvalidDateFormat) {
-		t.Fatalf("NewDateRange(\"2023-05-01\", \"Invalid Date Format\", 0), expected %s, got %s", ErrInvalidDateFormat, err)
+		t.Fatalf("NewDateRange(\"2023-05-01\", \"Invalid Date Format\", 0, 0), expected %s, got %s", ErrInvalidDateFormat, err)
 	}
 }
 
 func TestDateList(t *testing.T) {
-	r, _ := NewDateRange("2023-05-01", "2023-05-07", 0)
+	r, _ := NewDateRange("2023-05-01", "2023-05-07", 0, 0)
 	list := r.DateList()
 	if len(list) != 7 {
 		t.Fatalf("")
